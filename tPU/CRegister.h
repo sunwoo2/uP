355d70ae0b5e3dd180f8ed149131a300926c0162
sunwoo2 <abab702@naver.com>
2019-03-27 23:06:55 +0900

    Register

---
+++ tPU/CRegister.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class CRegister{
+    public:
+        CRegister() {}
+        virtual ~CRegister() {}
+};
+
+class C16RegisterFile : public CRegister {
+    public:
+        C16RegisterFile() {}
+        virtual ~C16RegisterFile() {}
+
+        void write_on_reg(unsigned int index, int data) { m_regs[index] = data; }
+        int read_from_reg(unsigned int index)           { return m_regs[index]; }
+
+		void show_regs();
+
+    private:
+        int m_regs[16];
+};
