1bd14d2ec3ad8eb6c98f965b6dc2d7f23675ff35
sunwoo2 <abab702@naver.com>
2019-03-29 14:21:20 +0900

    Execute Unit

--- tPU/CExecute.cpp
+++ tPU/CExecute.cpp
@@ -7,9 +7,17 @@ bool CT1ExecuteTinyUnit::do_execute() {
 
 	if(m_decode_unit.get_opcode() == MOV3){
 		cout << "Executing" << endl;
-		m_regs.write_on_reg(m_decode_unit.get_op1(), m_decode_unit.get_op2());
+
+		unsigned int reg_index = m_decode_unit.get_op1();
+				 int 	  data = m_decode_unit.get_op2();
+
+		m_regs.write_on_reg(reg_index, data);
+
 		return true;
+	}else{
+		cout << "Not executable instruction, not yet implemented sorry...!!" << endl;
+		return false;
 	}
 
-	return false;
+	return true;
 }
