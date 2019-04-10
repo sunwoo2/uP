d2152c76dfb8cf0583afbb3bbce1a0077fd02099
sunwoo2 <abab702@naver.com>
2019-04-10 20:26:26 +0900

    Register

--- tPU/CRegister.cpp
+++ tPU/CRegister.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void C16RegisterFile::show_regs(){
-	cout << "=======Register=======" << endl;
+	cout << "====== Register ======" << endl;
 	for(int i=0; i<16; i++)
 		if(i<10)
 			cout << "R" << i << "  = " << read_from_reg(i) << endl;
