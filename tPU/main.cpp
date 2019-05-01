64974bda51c4f2fa325c53a9c344e3d3950fb2cb
sunwoo2 <abab702@naver.com>
2019-05-01 14:11:06 +0900

    -

--- tPU/main.cpp
+++ tPU/main.cpp
@@ -27,26 +27,20 @@ int main(int argc, char* argv[]){
 	CSRAM_256W           mems;
 	CT1ExecuteTinyUnit 	 execute(decode, regs, mems);
 
-	for(int i=0; i<atoi(argv[2]); i++){
-		decode.do_fetch_from(i);
+    int size = atoi(argv[2]);
+	//for(int i=0; i<atoi(argv[2]); i++){
+    while( regs.get_PC() < size){
+        decode.do_fetch_from(regs.get_PC());
+		//decode.do_fetch_from(i);
 		decode.do_decode();
 		decode.show_instruction();
 		execute.do_execute();
 	}
+
 	regs.show_regs();
     mems.show_mems(0,9);
     mems.show_mems(250,255);
 
-    int decoded = 0;
-		decoded |= 128;	// b 1000 0000
-		decoded |= 64;	// b 0100 0000 
-		decoded |= 32;	// b 0010 0000 
-		decoded |= 16;	// b 0001 0000 
-		decoded |= 8;	// b 0000 1000
-		decoded |= 4;	// b 0000 0100
-		decoded |= 2;	// b 0000 0010
-		decoded |= 1;	// b 0000 0001
-    cout << decoded << endl;
 
 	return 0;
 }
