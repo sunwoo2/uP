ba89300ca2517b3f04b40e70e0f0a42731277d25
sunwoo2 <abab702@naver.com>
2019-05-10 20:30:40 +0900

    Decode Unit

--- tPU/CDecode.h
+++ tPU/CDecode.h
@@ -7,7 +7,7 @@ using namespace std;
 
 enum {R0=0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15};
 
-enum {MOV0=0,MOV1,MOV2,MOV3,ADD,SUB,JZ,MUL};
+enum {MOV0=0,MOV1,MOV2,MOV3,ADD,SUB,JZ,MUL,MOV4};
 
 class CDecode {
 	public:
